8cdecfde51dc8e93908c70ef0b50f30eedfe1c92
daniel <danielyknechtli@gmail.com>
2023-05-01 15:52:20 -0700

    adding 404 page

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require("express");   //this is needed/required to use express in node.js
-const app = express();    //this starts express, associates the name app with express 
+require("dotenv").config();
+
+const express = require("express"); //this is needed/required to use express in node.js
+const app = express(); //this starts express, associates the name app with express
+
 
 //this creates a default path, what the users sees in the browser when they just type the server name in the browser
-//in this case express will respond with the words "Hello World!" using the send function of the response 
-app.get("/", (req, res) => { 
+//in this case express will respond with the words "Hello World!" using the send function of the response
+app.get("/", (req, res) => {
   res.send("Hello world!"); //res is the response object, send is a function and "Hello World!" is the parameter to the send function
 });
 
-app.listen(3000); //now we tell express to listen for people contacting the server using the number 3000
+//this is used when the user types in an address that has not been defined
+//when an address user types has not been defined they will see the message 404 page
+app.get("*", (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+});  
+
+
+//now we tell express to listen for people contacting to the server using the number specified in the .env file with the name PORT
+//app refers to express, so we are saying to express to listen
+//the word process tells node.js to look at the .env file for the word PORT
+app.listen(process.env.PORT);
+
+
+
+
+
