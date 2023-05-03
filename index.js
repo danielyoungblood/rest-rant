5a17f0b88553ae85605e491fd33151768ff32f63
daniel <danielyknechtli@gmail.com>
2023-05-03 15:04:46 -0700

    adding 404 page

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use("/places", require("./controllers/places"));
 //in this case the server name is localhost:3000
 //in this case express will respond with the words "Hello World!" using the send function of the response
 app.get("/", (req, res) => {
-  res.render("home"); //res is the response object, send is a function and "Hello World!" is the parameter to the send function
+  res.render("home"); //res is the response object, render is a function and "home" refers to the file home.jsx
 });
 
 //this is used when the user types in an address that has not been defined
 //when an address user types has not been defined they will see the message 404 page
 //in other words the * means any number of characters, examples : test, api
 app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
+  res.render("error404");
 });
 
 //now we tell express to listen for people contacting to the server using the number specified in the .env file with the name PORT
