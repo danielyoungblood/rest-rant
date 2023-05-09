b8cee299405b6e8a390d4f886541e7b4087c06e8
daniel <danielyknechtli@gmail.com>
2023-05-08 17:21:21 -0700

    jazzed ip form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-//this establishes a route (path) to /places with using the GET method
+//this defines a route (path) to /places using the GET method
 //with express using the name router then a . followed by the http method defines a path
 router.get("/", (req, res) => {
   let places = [
@@ -22,7 +22,11 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
-//this establises a route path to /places/new
+router.post("/", (req, res) => {
+  res.send("POST /places - sent by express");
+});
+
+//this defines a route (path) to /places/new
 //router.get adds the "/places" to this path that ends with "/new" giving us the complete path as "/places/new"
 router.get("/new", (req, res) => {
   res.render("places/new");
